e08e90d5685ecb478834106a5293c5110962f653
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2019-01-05 16:45:22 -0800

    added commands and subsystems folder

---
+++ src/main/java/org/usfirst/frc4904/robot/commands/ExampleCommand.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc4904.robot.commands;
+
+import org.usfirst.frc4904.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ExampleCommand extends Command {
+	public ExampleCommand() {
+		// Use requires() here to declare subsystem dependencies
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+		System.out.println("The command has initialized!");
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		// Do you want to set the speed of a motor here? Check this out:
+		// https://github.com/RoboticsTeam4904/standard/blob/cc66c21f46c1dd15b7150a66ae2e85cd06ef8f72/subsystems/motor/Motor.java#L231
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+	}
+}
