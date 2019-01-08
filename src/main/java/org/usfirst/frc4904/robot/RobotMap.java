5f7a28df50a3de5e9a326000cda72e3fa77db782
Evan Steirman <evastei@nus11172-10-evastei.nueva.local>
2019-01-07 17:33:00 -0800

    changed some PID values

--- src/main/java/org/usfirst/frc4904/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc4904/robot/RobotMap.java
@@ -24,24 +24,24 @@ public class RobotMap {
 			public static final double I = 0.0000000004904;
 			public static final double D = 0.4904;
 			public static final double F = 0.00004904;
-			public static final double tolerance = 0.4904;
-			public static final double dTolerance = 0.4904;
+			public static final double tolerance = 4.904;
+			public static final double dTolerance = 4.904;
         }
         public static class Turn{
-            public static final double P = 0.002;
-			public static final double I = 0.0;
-			public static final double D = -0.05;
-			public static final double F = 0.2;
-			public static final double tolerance = 1.0;
-			public static final double dTolerance = 0.1;
+            public static final double P = 0.00004904;
+			public static final double I = 0.0000000004904;
+			public static final double D = 0.4904;
+			public static final double F = 0.00004904;
+			public static final double tolerance = 4.904;
+			public static final double dTolerance = 4.904;
         }
         public static class Elevator{
-            public static final double P = 0.002;
-			public static final double I = 0.0;
-			public static final double D = -0.05;
-			public static final double F = 0.2;
-			public static final double tolerance = 1.0;
-			public static final double dTolerance = 0.1;
+            public static final double P = 0.00004904;
+			public static final double I = 0.0000000004904;
+			public static final double D = 0.4904;
+			public static final double F = 0.00004904;
+			public static final double tolerance = 4.904;
+			public static final double dTolerance = 4.904;
         }
     }
     public static class Component {
