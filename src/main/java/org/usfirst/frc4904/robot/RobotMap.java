f9c58137051bd2f0d1db721bde9b27a1e87d4f2c
lauwong <lauwong@nuevaschool.org>
2019-01-10 17:08:20 -0800

    Changed indentation from spaces to tabs

--- src/main/java/org/usfirst/frc4904/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc4904/robot/RobotMap.java
@@ -9,11 +9,11 @@ import org.usfirst.frc4904.standard.custom.motioncontrollers.CANTalonSRX;
 public class RobotMap {
     public static class Port {
         public static class HumanInput {
-            public static final int xboxController = 1;
+			public static final int xboxController = 1;
 
         }
         public static class CANMotor {
-            public static final int cargoIORollerMotorLeft = 1;
+			public static final int cargoIORollerMotorLeft = 1;
 			public static final int cargoIORollerMotorRight = 7;
         }
         public static class PWM {}
@@ -23,21 +23,21 @@ public class RobotMap {
     
     public static class Metrics{}
     public static class Component {
-        public static CustomXbox driverXbox;
-        public static CargoIO cargoIO;
-        public static Motor cargoIORollerLeft;
+		public static CustomXbox driverXbox;
+		public static CargoIO cargoIO;
+		public static Motor cargoIORollerLeft;
 		public static Motor cargoIORollerRight;
     }
     public static class HumanInput {
         public static class Driver {
-            public static CustomXbox xbox;
+			public static CustomXbox xbox;
         }
         public static class Operator {}
     }
     public RobotMap() {
-        Component.driverXbox = new CustomXbox(Port.HumanInput.xboxController);
-        Component.driverXbox.setDeadZone(0.1);
-        Component.cargoIORollerLeft = new Motor("CargoIORollerLeft", new CANTalonSRX(Port.CANMotor.cargoIORollerMotorLeft));
+		Component.driverXbox = new CustomXbox(Port.HumanInput.xboxController);
+		Component.driverXbox.setDeadZone(0.1);
+		Component.cargoIORollerLeft = new Motor("CargoIORollerLeft", new CANTalonSRX(Port.CANMotor.cargoIORollerMotorLeft));
 		Component.cargoIORollerRight = new Motor("CargoIORollerRight", new CANTalonSRX(Port.CANMotor.cargoIORollerMotorRight));
 		Component.cargoIO = new CargoIO(Component.cargoIORollerLeft, Component.cargoIORollerRight);
     }
