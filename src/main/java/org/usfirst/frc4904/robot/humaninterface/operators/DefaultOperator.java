814e01052671588202d5915b2543f85a9a7a281d
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2019-01-11 18:56:42 -0800

    isolating change

--- src/main/java/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
+++ src/main/java/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
@@ -1,7 +1,9 @@
 package org.usfirst.frc4904.robot.humaninterface.operators;
 
+
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.standard.humaninput.Operator;
+
 public class DefaultOperator extends Operator {
 	public DefaultOperator() {
 		super("DefaultOperator");
