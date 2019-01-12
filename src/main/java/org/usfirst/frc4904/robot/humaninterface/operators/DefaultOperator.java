1a17daa55acbcfe26be8e4e938410561e337fb57
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2019-01-11 18:48:34 -0800

    Update DefaultOperator.java

--- src/main/java/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
+++ src/main/java/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc4904.robot.humaninterface.operators;
 
 import org.usfirst.frc4904.standard.humaninput.Operator;
-
+import org.usfirst.frc4904.robot.RobotMap;
 public class DefaultOperator extends Operator {
 	public DefaultOperator() {
 		super("DefaultOperator");
