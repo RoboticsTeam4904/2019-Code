a806e2ada60e09a291c79aa92b58ae3d93a78121
elkia-federation <erjung12@yahoo.com>
2019-01-13 11:47:56 -0800

    add Tim subsystem wrapper for 3 motors and associated commands

--- src/main/java/org/usfirst/frc4904/robot/commands/TimRotate.java
+++ src/main/java/org/usfirst/frc4904/robot/commands/TimRotate.java
@@ -3,7 +3,7 @@ package org.usfirst.frc4904.robot.commands;
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.standard.commands.motor.MotorPositionConstant;
 
-
+import edu.wpi.first.wpilibj.command.Command;
 
 public class TimRotate extends MotorPositionConstant {
 	public TimRotate(double position) {
