1c3c03af3c466ebc63b7159da26a603d06ed6d27
josehrl <joshuatehrlich@gmail.com>
2019-01-14 17:50:49 -0800

    Elevator code

--- src/main/java/org/usfirst/frc4904/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc4904/robot/RobotMap.java
@@ -1,6 +1,12 @@
 package org.usfirst.frc4904.robot;
 
+import org.usfirst.frc4904.robot.subsystems.Elevator;
 import org.usfirst.frc4904.standard.custom.controllers.CustomXbox;
+import org.usfirst.frc4904.standard.custom.motioncontrollers.CANTalonSRX;
+import org.usfirst.frc4904.standard.custom.motioncontrollers.CustomPIDController;
+import org.usfirst.frc4904.standard.custom.motioncontrollers.MotionController;
+import org.usfirst.frc4904.standard.custom.sensors.CANEncoder;
+import org.usfirst.frc4904.standard.subsystems.motor.Motor;
 
 
 public class RobotMap {
@@ -9,15 +15,34 @@ public class RobotMap {
             public static final int xboxController = 1;
 
         }
-        public static class CANMotor {}
+        public static class CANMotor {
+            public static final int elevMotor1 = 1; // not final numbers
+            public static final int elevMotor2 = 2;
+        }
         public static class PWM {}
-        public static class CAN {}
+        public static class CAN {
+            public static final int elevCANEncoder = 1; // also not final #
+        }
         public static class Pneumatics {}
     }
     
     public static class Metrics{}
+    public static class PID{
+        public static class Elevator {
+            public static final double P = 1; // ALSO not final number
+            public static final double I = 1;
+            public static final double D = 1;
+            public static final double F = 1;
+        }
+    }
     public static class Component {
         public static CustomXbox driverXbox;
+
+        public static Elevator elevator;
+        public static CANTalonSRX elevMotor1;
+        public static CANTalonSRX elevMotor2;
+        public static CANEncoder elevCANEncoder;
+        public static CustomPIDController elevPID;
     }
     public static class HumanInput {
         public static class Driver {
@@ -27,6 +52,13 @@ public class RobotMap {
     }
     public RobotMap() {
         Component.driverXbox = new CustomXbox(Port.HumanInput.xboxController);
-		Component.driverXbox.setDeadZone(0.1);
+        Component.driverXbox.setDeadZone(0.1);
+        
+        Component.elevMotor1 = new CANTalonSRX(Port.CANMotor.elevMotor1);
+        Component.elevMotor2 = new CANTalonSRX(Port.CANMotor.elevMotor2);
+        Component.elevCANEncoder = new CANEncoder(Port.CAN.elevCANEncoder);
+        Component.elevPID = new CustomPIDController(PID.Elevator.P, PID.Elevator.I, PID.Elevator.D, PID.Elevator.F, Component.elevCANEncoder);
+        Component.elevator = new Elevator(Component.elevPID, Component.elevCANEncoder, Component.elevMotor1, Component.elevMotor2);
+
     }
 }
---
+++ src/main/java/org/usfirst/frc4904/robot/subsystems/Elevator.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc4904.robot.subsystems;
+
+import org.usfirst.frc4904.standard.commands.motor.MotorIdle;
+import org.usfirst.frc4904.standard.custom.motioncontrollers.MotionController;
+import org.usfirst.frc4904.standard.custom.sensors.CANEncoder;
+import org.usfirst.frc4904.standard.subsystems.motor.Motor;
+import org.usfirst.frc4904.standard.subsystems.motor.PositionSensorMotor;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Elevator extends PositionSensorMotor {
+	public static final double UP_SPEED = 0.5;
+	public static final double DOWN_SPEED = -0.5;
+	public static final double ENCODER_TICKS = 1024; //todo: change to real thing
+	public static final double TICK_MULTIPLIER = 360/ENCODER_TICKS;
+	public final CANEncoder elevEncoder;
+
+	public enum ElevState {
+		UP(10), MIDDLE(5), DOWN(0); // TODO: NEED TO BE TWEAKED A LOOOOT, arbitrary numbers rn
+		public final double position;
+
+		private ElevState(double position) {
+			this.position = position;
+		}
+	}
+
+	public Elevator (MotionController motionController, CANEncoder elevEncoder, SpeedController... speedControllers) {
+		super("Elevator", motionController, speedControllers);
+		this.elevEncoder = elevEncoder;
+		this.elevEncoder.setDistancePerPulse(TICK_MULTIPLIER);
+	}
+
+	public void setPosition (double position) {
+		super.setPosition(position);
+	}
+
+	public void setPosition (ElevState state) {
+		setPosition(state.position);
+	}
+
+	public void set (double speed) {
+		super.set(speed);
+	}
+}
