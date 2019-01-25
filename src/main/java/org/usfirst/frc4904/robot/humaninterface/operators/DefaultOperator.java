a50a9b6180bbbc9681128f3e28abc4c69783ba29
josehrl <joshuatehrlich@gmail.com>
2019-01-24 17:21:24 -0800

    added code to stop elevator from going to far when being manually controlled, and an override to let the driver break the robot anyway.

--- src/main/java/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
+++ src/main/java/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
@@ -2,11 +2,14 @@ package org.usfirst.frc4904.robot.humaninterface.operators;
 
 
 import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.robot.commands.ElevatorSetSpeed;
 import org.usfirst.frc4904.standard.humaninput.Operator;
 import org.usfirst.frc4904.standard.custom.controllers.CustomJoystick;
 import org.usfirst.frc4904.standard.commands.RunIfElse;
 import org.usfirst.frc4904.standard.commands.motor.MotorControl;
 import org.usfirst.frc4904.robot.subsystems.Elevator;
+import org.usfirst.frc4904.standard.commands.OverrideEnable;
+import org.usfirst.frc4904.standard.commands.OverrideDisable;
 
 
 public class DefaultOperator extends Operator {
@@ -20,15 +23,8 @@ public class DefaultOperator extends Operator {
 
 	@Override
 	public void bindCommands() {
-
-		RobotMap.HumanInput.Operator.joystick.button1.onlyWhileHeld(new RunIfElse(
-            new MotorControl(RobotMap.Component.elevator, RobotMap.HumanInput.Operator.joystick, CustomJoystick.Y_AXIS,
-                Elevator.UP_SPEED),
-            new MotorControl(RobotMap.Component.elevator, RobotMap.HumanInput.Operator.joystick, CustomJoystick.Y_AXIS,
-                Elevator.DOWN_SPEED),
-            () -> {
-                return RobotMap.HumanInput.Operator.joystick.getAxis(CustomJoystick.Y_AXIS) > 0;
-            }));
-
+		RobotMap.HumanInput.Operator.joystick.button1.onlyWhileHeld(new ElevatorSetSpeed(RobotMap.HumanInput.Operator.joystick));
+		RobotMap.HumanInput.Operator.joystick.button2.onlyWhileHeld(new OverrideEnable(RobotMap.Component.elevator));
+		RobotMap.HumanInput.Operator.joystick.button2.whenReleased(new OverrideDisable(RobotMap.Component.elevator));
 	}
 }
--- src/main/java/org/usfirst/frc4904/robot/subsystems/Elevator.java
+++ src/main/java/org/usfirst/frc4904/robot/subsystems/Elevator.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc4904.robot.subsystems;
 
 import org.usfirst.frc4904.standard.commands.motor.MotorIdle;
+import org.usfirst.frc4904.standard.custom.Overridable;
 import org.usfirst.frc4904.standard.custom.motioncontrollers.MotionController;
 import org.usfirst.frc4904.standard.custom.sensors.CANEncoder;
 import org.usfirst.frc4904.standard.subsystems.motor.Motor;
@@ -9,14 +10,17 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class Elevator extends PositionSensorMotor {
+public class Elevator extends PositionSensorMotor implements Overridable {
 	public static final double UP_SPEED = 0.5;
 	public static final double DOWN_SPEED = -0.5;
 	public static final double ENCODER_TICKS = 1024;
 	public static final double DISTANCE_PER_ROTATION = 0; //SET LATER
 	public static final double TICK_MULTIPLIER = DISTANCE_PER_ROTATION;
+	public static final double MIN_POSITION = 0;
+	public static final double MAX_POSITION = 500; // ARBITRATY NUMBER, FIND OUT LATER
 	public final CANEncoder elevEncoder;
 	public ElevState state = ElevState.UP;
+	public boolean isOverridden;
 
 	public enum ElevState {
 		UP(10), MIDDLE(5), DOWN(0); // TODO: NEED TO BE TWEAKED A LOOOOT, arbitrary numbers rn
@@ -46,5 +50,11 @@ public class Elevator extends PositionSensorMotor {
 	}
 	public double getPosition() {
 		return state.position;
+	}
+	public void setOverridden(boolean override) {
+		this.isOverridden = override;
+	}
+	public boolean isOverridden() {
+		return isOverridden;
 	}
 }
