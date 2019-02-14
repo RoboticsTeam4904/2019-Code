3cfb65d2612ff10e19af771f555f8cf276d0074e
Evan Steirman <evastei@nuevaschool.org>
2019-02-13 21:50:38 -0800

    fixed up/added to the math

--- src/main/java/org/usfirst/frc4904/robot/autonly/InterestingAuton.java
+++ src/main/java/org/usfirst/frc4904/robot/autonly/InterestingAuton.java
@@ -10,9 +10,9 @@ public class InterestingAuton extends CommandGroup {
 
 	public InterestingAuton(double beta, double x, double y) {
 		super("InterestingAuton");
-		double firstDrive = x/Math.sin(beta);
-		double newY = y - x/Math.tan(beta);
 		if (x >= 0) {
+			double firstDrive = x/Math.sin(-beta);
+			double newY = y - x/Math.tan(-beta);
 			if (newY <= 0 || newY >= y) {
 				addSequential(new ChassisTurn(RobotMap.Component.chassis, 90 + beta, RobotMap.Component.navx, null, RobotMap.Component.chassisTurnMC));
 				addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, x, RobotMap.Component.drivePID));
@@ -26,10 +26,19 @@ public class InterestingAuton extends CommandGroup {
 			}
 		}
 		else {
-			addSequential(new ChassisTurn(RobotMap.Component.chassis, -90 + beta, RobotMap.Component.navx, null, RobotMap.Component.chassisTurnMC));
-			addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, -x, RobotMap.Component.drivePID));
-			addSequential(new ChassisTurn(RobotMap.Component.chassis, 90, RobotMap.Component.navx, null, RobotMap.Component.chassisTurnMC));
-			addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, y, RobotMap.Component.drivePID));
+			double firstDrive = x/Math.sin(beta);
+			double newY = y - x/Math.tan(beta);
+			if (newY <= 0 || newY >= y) {
+				addSequential(new ChassisTurn(RobotMap.Component.chassis, -90 + beta, RobotMap.Component.navx, null, RobotMap.Component.chassisTurnMC));
+				addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, -x, RobotMap.Component.drivePID));
+				addSequential(new ChassisTurn(RobotMap.Component.chassis, 90, RobotMap.Component.navx, null, RobotMap.Component.chassisTurnMC));
+				addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, y, RobotMap.Component.drivePID));
+			}
+			else {
+				addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, firstDrive, RobotMap.Component.drivePID));
+				addSequential(new ChassisTurn(RobotMap.Component.chassis, beta, RobotMap.Component.navx, null, RobotMap.Component.chassisTurnMC));
+				addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, newY, RobotMap.Component.drivePID));
+			}
 		}
 	}
 }
