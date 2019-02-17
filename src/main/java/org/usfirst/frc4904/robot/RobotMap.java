b8900041ad7bfb8a7df28779de79ea4b737c148f
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2019-02-17 12:03:48 -0800

    PR fixes

--- src/main/java/org/usfirst/frc4904/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc4904/robot/RobotMap.java
@@ -19,135 +19,129 @@ import org.usfirst.frc4904.standard.custom.sensors.NavX;
 import edu.wpi.first.wpilibj.SerialPort;
 
 public class RobotMap {
-    public static class Port {
-        public static class HumanInput {
-            public static final int joystick = 0;
-            public static final int xboxController = 1;
-        }
+	public static class Port {
+		public static class HumanInput {
+			public static final int joystick = 0;
+			public static final int xboxController = 1;
+		}
 
-        public static class CANMotor {
-            public static final int leftDriveA = 24; //TODO: MIGHT CHANGE
-            public static final int leftDriveB = 2;
-            public static final int rightDriveA = 3;
-            public static final int rightDriveB = 4;
-        }
+		public static class CANMotor {
+			public static final int leftDriveA = -1; // TODO: CHANGE PORTS
+			public static final int leftDriveB = -1;
+			public static final int rightDriveA = -1;
+			public static final int rightDriveB = -1;
+		}
 
-        public static class PWM {
-        }
+		public static class CAN {
+			public static final int leftEncoder = -1; // TODO: CHANGE PORTS
+			public static final int rightEncoder = -1;
+		}
 
-        public static class CAN {
-            public static final int leftEncoder = -1; //TODO: CHANGE PORTS
-            public static final int rightEncoder = -1;
-        }
+		public static class Pneumatics {
+			public static final PCMPort shifter = new PCMPort(-1, -1, -1); // TODO: CHANGE PORTS
+		}
+	}
 
-        public static class Pneumatics {
-            public static final PCMPort shifter = new PCMPort(-1, -1, -1); //TODO: CHANGE PORTS
-        }
-    }
+	public static class Metrics {
+		public static class Wheel {
+			public static final double TICKS_PER_REVOLUTION = -1; // TODO: CHANGE CONSTS
+			public static final double DIAMETER_INCHES = -1;
+			public static final double CIRCUMFERENCE_INCHES = Metrics.Wheel.DIAMETER_INCHES * Math.PI;
+			public static final double TICKS_PER_INCH = Metrics.Wheel.TICKS_PER_REVOLUTION
+				/ Metrics.Wheel.CIRCUMFERENCE_INCHES;
+			public static final double DISTANCE_FRONT_BACK = -1;
+			public static final double DISTANCE_SIDE_SIDE = -1;
+			public static final double INCHES_PER_TICK = Metrics.Wheel.CIRCUMFERENCE_INCHES
+				/ Metrics.Wheel.TICKS_PER_REVOLUTION;
+		}
+	}
 
-    public static class Metrics {
-        public static class Wheel {
-            public static final double TICKS_PER_REVOLUTION = -1; //TODO: CHANGE CONSTS
-            public static final double DIAMETER_INCHES = -1;
-            public static final double CIRCUMFERENCE_INCHES = Metrics.Wheel.DIAMETER_INCHES * Math.PI;
-            public static final double TICKS_PER_INCH = Metrics.Wheel.TICKS_PER_REVOLUTION
-                / Metrics.Wheel.CIRCUMFERENCE_INCHES;
-            public static final double DISTANCE_FRONT_BACK = -1;
-            public static final double DISTANCE_SIDE_SIDE = -1;
-            public static final double INCHES_PER_TICK = Metrics.Wheel.CIRCUMFERENCE_INCHES
-                / Metrics.Wheel.TICKS_PER_REVOLUTION;
-        }
-    }
+	public static class PID {
+		public static class Drive {
+			public static final double P = -1; // TODO: TUNE
+			public static final double I = -1;
+			public static final double D = -1;
+			public static final double F = -1;
+			public static final double tolerance = -1;
+			public static final double dTolerance = -1;
+		}
 
-    public static class PID {
-        public static class Drive {
-            public static final double P = -1; // TODO: TUNE
-            public static final double I = -1;
-            public static final double D = -1;
-            public static final double F = -1;
-            public static final double tolerance = -1;
-            public static final double dTolerance = -1;
-        }
+		public static class Turn {
+			public static final double P = -1; // TODO: TUNE
+			public static final double I = -1;
+			public static final double D = -1;
+			public static final double F = -1;
+			public static final double tolerance = -1;
+			public static final double dTolerance = -1;
+		}
+	}
 
-        public static class Turn {
-            public static final double P = -1; // TODO: TUNE
-            public static final double I = -1;
-            public static final double D = -1;
-            public static final double F = -1;
-            public static final double tolerance = -1;
-            public static final double dTolerance = -1;
-        }
-    }
+	public static class Component {
+		public static PDP pdp;
+		public static TankDriveShifting chassis;
+		public static Motor leftWheelA;
+		public static Motor leftWheelB;
+		public static Motor rightWheelA;
+		public static Motor rightWheelB;
+		public static SolenoidShifters shifter;
+		public static EnableableModifier rightWheelAccelerationCap;
+		public static EnableableModifier leftWheelAccelerationCap;
+		public static CANEncoder leftWheelEncoder;
+		public static CANEncoder rightWheelEncoder;
+		public static EncoderPair chassisEncoders;
+		public static CustomPIDController chassisTurnPID;
+		public static CustomPIDController drivePID;
+		public static NavX navx;
+	}
 
-    public static class Component {
-        public static PDP pdp;
-        public static TankDriveShifting chassis;
-        public static Motor leftWheelA;
-        public static Motor leftWheelB;
-        public static Motor rightWheelA;
-        public static Motor rightWheelB;
-        public static SolenoidShifters shifter;
-        public static EnableableModifier rightWheelAccelerationCap;
-        public static EnableableModifier leftWheelAccelerationCap;
-        public static CANEncoder leftWheelEncoder;
-        public static CANEncoder rightWheelEncoder;
-        public static EncoderPair chassisEncoders;
-        public static CustomPIDController chassisTurnPID;
-        public static CustomPIDController drivePID;
-        public static NavX navx;
-    }
+	public static class HumanInput {
+		public static class Driver {
+			public static CustomXbox xbox;
+		}
 
-    public static class HumanInput {
-        public static class Driver {
-            public static CustomXbox xbox;
-        }
+		public static class Operator {
+			public static CustomJoystick joystick;
+		}
+	}
 
-        public static class Operator {
-            public static CustomJoystick joystick;
-        }
-    }
-
-    public RobotMap() {
-        /* General */
-        Component.pdp = new PDP();
-        Component.navx = new NavX(SerialPort.Port.kMXP);
-        /* Drive Train */
-        // Wheel Encoders
-        Component.leftWheelEncoder = new CANEncoder("LeftEncoder", Port.CAN.leftEncoder);
-        Component.rightWheelEncoder = new CANEncoder("RightEncoder", Port.CAN.rightEncoder);
-        Component.chassisEncoders = new EncoderPair(Component.leftWheelEncoder, Component.rightWheelEncoder);
-        Component.leftWheelEncoder.setDistancePerPulse(Metrics.Wheel.INCHES_PER_TICK);
-        Component.rightWheelEncoder.setDistancePerPulse(Metrics.Wheel.INCHES_PER_TICK);
-        Component.leftWheelAccelerationCap = new EnableableModifier(new AccelerationCap(Component.pdp));
-        Component.leftWheelAccelerationCap.enable();
-        Component.rightWheelAccelerationCap = new EnableableModifier(new AccelerationCap(Component.pdp));
-        Component.rightWheelAccelerationCap.enable();
-        // Wheels
-        Component.rightWheelA = new Motor("rightWheelA", true, Component.rightWheelAccelerationCap,
-            new CANTalonSRX(Port.CANMotor.rightDriveA));
-        Component.rightWheelB = new Motor("rightWheelB", Component.rightWheelAccelerationCap,
-            new CANTalonSRX(Port.CANMotor.rightDriveB));
-        Component.leftWheelA = new Motor("leftWheelA", true, Component.leftWheelAccelerationCap,
-            new CANTalonSRX(Port.CANMotor.leftDriveA));
-        Component.leftWheelB = new Motor("leftWheelB", true, Component.leftWheelAccelerationCap,
-            new CANTalonSRX(Port.CANMotor.leftDriveB));
-        // Shifter
-        Component.shifter = new SolenoidShifters(Port.Pneumatics.shifter.pcmID, Port.Pneumatics.shifter.forward,
-            Port.Pneumatics.shifter.reverse);
-        // General Chassis
-        Component.chassis = new TankDriveShifting("2019-Chassis", Component.leftWheelA, Component.leftWheelB,
-            Component.rightWheelA, Component.rightWheelB, Component.shifter);
-        Component.drivePID = new CustomPIDController(PID.Drive.P, PID.Drive.I, PID.Drive.D, PID.Drive.F,
-            Component.rightWheelEncoder);
-        Component.drivePID.setAbsoluteTolerance(PID.Drive.tolerance);
-        Component.drivePID.setDerivativeTolerance(PID.Drive.dTolerance);
-        Component.chassisTurnPID = new CustomPIDController(PID.Turn.P, PID.Turn.I, PID.Turn.D, Component.navx);
-        Component.chassisTurnPID.setAbsoluteTolerance(PID.Turn.tolerance);
-        Component.chassisTurnPID.setDerivativeTolerance(PID.Turn.dTolerance);
-        // Human Input
-        HumanInput.Driver.xbox = new CustomXbox(Port.HumanInput.xboxController);
-        HumanInput.Driver.xbox.setDeadZone(HumanInterfaceConfig.XBOX_DEADZONE);
-        HumanInput.Operator.joystick = new CustomJoystick(Port.HumanInput.joystick);
-        HumanInput.Operator.joystick.setDeadzone(HumanInterfaceConfig.JOYSTICK_DEADZONE);
-    }
+	public RobotMap() {
+		/* General */
+		Component.pdp = new PDP();
+		Component.navx = new NavX(SerialPort.Port.kMXP);
+		/* Drive Train */
+		// Wheel Encoders
+		Component.leftWheelEncoder = new CANEncoder("LeftEncoder", Port.CAN.leftEncoder, Metrics.Wheel.INCHES_PER_TICK);
+		Component.rightWheelEncoder = new CANEncoder("RightEncoder", Port.CAN.rightEncoder, Metrics.Wheel.INCHES_PER_TICK);
+		Component.chassisEncoders = new EncoderPair(Component.leftWheelEncoder, Component.rightWheelEncoder);
+		Component.leftWheelAccelerationCap = new EnableableModifier(new AccelerationCap(Component.pdp));
+		Component.leftWheelAccelerationCap.enable();
+		Component.rightWheelAccelerationCap = new EnableableModifier(new AccelerationCap(Component.pdp));
+		Component.rightWheelAccelerationCap.enable();
+		// Wheels
+		Component.rightWheelA = new Motor("rightWheelA", true, Component.rightWheelAccelerationCap,
+			new CANTalonSRX(Port.CANMotor.rightDriveA));
+		Component.rightWheelB = new Motor("rightWheelB", Component.rightWheelAccelerationCap,
+			new CANTalonSRX(Port.CANMotor.rightDriveB));
+		Component.leftWheelA = new Motor("leftWheelA", true, Component.leftWheelAccelerationCap,
+			new CANTalonSRX(Port.CANMotor.leftDriveA));
+		Component.leftWheelB = new Motor("leftWheelB", true, Component.leftWheelAccelerationCap,
+			new CANTalonSRX(Port.CANMotor.leftDriveB));
+		// Shifter
+		Component.shifter = new SolenoidShifters(Port.Pneumatics.shifter.buildDoubleSolenoid());
+		// General Chassis
+		Component.chassis = new TankDriveShifting("2019-Chassis", Component.leftWheelA, Component.leftWheelB,
+			Component.rightWheelA, Component.rightWheelB, Component.shifter);
+		Component.drivePID = new CustomPIDController(PID.Drive.P, PID.Drive.I, PID.Drive.D, PID.Drive.F,
+			Component.chassisEncoders);
+		Component.drivePID.setAbsoluteTolerance(PID.Drive.tolerance);
+		Component.drivePID.setDerivativeTolerance(PID.Drive.dTolerance);
+		Component.chassisTurnPID = new CustomPIDController(PID.Turn.P, PID.Turn.I, PID.Turn.D, Component.navx);
+		Component.chassisTurnPID.setAbsoluteTolerance(PID.Turn.tolerance);
+		Component.chassisTurnPID.setDerivativeTolerance(PID.Turn.dTolerance);
+		// Human Input
+		HumanInput.Driver.xbox = new CustomXbox(Port.HumanInput.xboxController);
+		HumanInput.Driver.xbox.setDeadZone(HumanInterfaceConfig.XBOX_DEADZONE);
+		HumanInput.Operator.joystick = new CustomJoystick(Port.HumanInput.joystick);
+		HumanInput.Operator.joystick.setDeadzone(HumanInterfaceConfig.JOYSTICK_DEADZONE);
+	}
 }
