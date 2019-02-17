be9c9b9754776e63f571051934d1c6c15e797067
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2019-02-17 11:55:25 -0800

    PR changes

--- src/main/java/org/usfirst/frc4904/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc4904/robot/RobotMap.java
@@ -1,46 +1,32 @@
 package org.usfirst.frc4904.robot;
 
-
-import edu.wpi.first.wpilibj.I2C;
 import org.usfirst.frc4904.standard.custom.controllers.CustomXbox;
+
 
 public class RobotMap {
     public static class Port {
         public static class HumanInput {
             public static final int xboxController = 1;
-        }
 
-        public static class CANMotor {
         }
-
-        public static class PWM {
-        }
-
-        public static class CAN {
-        }
-
-        public static class Pneumatics {
-        }
+        public static class CANMotor {}
+        public static class PWM {}
+        public static class CAN {}
+        public static class Pneumatics {}
     }
-
-    public static class Metrics {
-    }
-
+    
+    public static class Metrics{}
     public static class Component {
         public static CustomXbox driverXbox;
     }
-
     public static class HumanInput {
         public static class Driver {
             public static CustomXbox xbox;
         }
-
-        public static class Operator {
-        }
+        public static class Operator {}
     }
-
     public RobotMap() {
         Component.driverXbox = new CustomXbox(Port.HumanInput.xboxController);
-        Component.driverXbox.setDeadZone(0.1);
+		Component.driverXbox.setDeadZone(0.1);
     }
 }
--- src/main/java/org/usfirst/frc4904/robot/commands/CameraStream.java
+++ src/main/java/org/usfirst/frc4904/robot/commands/CameraStream.java
@@ -1,8 +1,7 @@
 package org.usfirst.frc4904.robot.commands;
 
-import java.util.ArrayList;
 
-import org.usfirst.frc4904.standard.LogKitten;
+import java.util.ArrayList;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.cscore.VideoMode;
 import edu.wpi.cscore.VideoSink;
@@ -14,14 +13,14 @@ import edu.wpi.first.wpilibj.command.Command;
  * Streams with multiple cameras
  */
 public class CameraStream extends Command {
-    private final int streamingXRes = 320;
-    private final int streamingYRes = 240;
-    private final int streamingFPS = 15;
-    private final VideoMode.PixelFormat streamingFmt = VideoMode.PixelFormat.kMJPEG;
+    private static final int STREAMING_X_RES = 320;
+    private static final int STREAMING_Y_RES = 240;
+    private static final int STREAMING_FPS = 15;
+    private static final VideoMode.PixelFormat streamingFmt = VideoMode.PixelFormat.kMJPEG;
     private final VideoSink server;
     private final ArrayList<UsbCamera> cameras;
 
-    public CameraStream(int numCameras) {
+    public CameraStream(int numCameras, int streamingXRes, int streamingYRes, int streamingFPS) {
         cameras = new ArrayList<>(numCameras);
         for (int i = 0; i < numCameras; ++i) {
             cameras.add(CameraServer.getInstance().startAutomaticCapture(i));
@@ -31,24 +30,12 @@ public class CameraStream extends Command {
         server = CameraServer.getInstance().getServer();
     }
 
-    @Override
-    protected void initialize() {
-    }
-
-    @Override
-    protected void execute() {
+    public CameraStream(int numCameras) {
+        this(numCameras, STREAMING_X_RES, STREAMING_Y_RES, STREAMING_FPS);
     }
 
     @Override
     protected boolean isFinished() {
         return false;
-    }
-
-    @Override
-    protected void end() {
-    }
-
-    @Override
-    protected void interrupted() {
     }
 }
