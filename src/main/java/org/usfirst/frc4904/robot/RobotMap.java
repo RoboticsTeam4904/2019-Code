ee62b7964c1db12d57a37e472e68d48b654e87f7
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2019-03-03 13:44:43 -0800

    comments

--- src/main/java/org/usfirst/frc4904/robot/RobotMap.java
+++ src/main/java/org/usfirst/frc4904/robot/RobotMap.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc4904.robot;
 
+
 import org.usfirst.frc4904.robot.subsystems.FloorIO;
 import org.usfirst.frc4904.standard.subsystems.SolenoidSubsystem;
 import org.usfirst.frc4904.standard.subsystems.SolenoidSubsystem.SolenoidState;
@@ -55,14 +56,12 @@ public class RobotMap {
 			public static final PCMPort velcroPiston = new PCMPort(0, 1, 0); // TODO: Adjust port numbers
 			public static final PCMPort hatchOuttakePiston = new PCMPort(0, 3, 2); // TODO: Adjust port numbers
 			public static final PCMPort wrist = new PCMPort(-1, -1, -1); // TODO: Adjust port numbers
-
 		}
 
 		public static class Digital {
 			public static final int elevatorSwitchBottomPort = 0;
 			public static final int elevatorSwitchTopPort = 1;
 		}
-
 	}
 
 	public static class Metrics {
@@ -71,11 +70,11 @@ public class RobotMap {
 			public static final double DIAMETER_INCHES = -1;
 			public static final double CIRCUMFERENCE_INCHES = Metrics.Chassis.DIAMETER_INCHES * Math.PI;
 			public static final double TICKS_PER_INCH = Metrics.Chassis.TICKS_PER_REVOLUTION
-					/ Metrics.Chassis.CIRCUMFERENCE_INCHES;
+				/ Metrics.Chassis.CIRCUMFERENCE_INCHES;
 			public static final double DISTANCE_FRONT_BACK = -1;
 			public static final double DISTANCE_SIDE_SIDE = -1;
 			public static final double INCHES_PER_TICK = Metrics.Chassis.CIRCUMFERENCE_INCHES
-					/ Metrics.Chassis.TICKS_PER_REVOLUTION;
+				/ Metrics.Chassis.TICKS_PER_REVOLUTION;
 		}
 	}
 
@@ -106,7 +105,6 @@ public class RobotMap {
 			public static final double tolerance = 4.0;
 			public static final double dTolerance = 3.0;
 			// public static final double IThreshold = 13;
-
 		}
 	}
 
@@ -157,9 +155,9 @@ public class RobotMap {
 		/* Drive Train */
 		// Wheel Encoders
 		Component.leftWheelEncoder = new CANEncoder("LeftEncoder", Port.CAN.leftWheelEncoder,
-				Metrics.Chassis.INCHES_PER_TICK);
+			Metrics.Chassis.INCHES_PER_TICK);
 		Component.rightWheelEncoder = new CANEncoder("RightEncoder", Port.CAN.rightWheelEncoder,
-				Metrics.Chassis.INCHES_PER_TICK);
+			Metrics.Chassis.INCHES_PER_TICK);
 		Component.chassisEncoders = new EncoderPair(Component.leftWheelEncoder, Component.rightWheelEncoder);
 		Component.leftWheelAccelerationCap = new EnableableModifier(new AccelerationCap(Component.pdp));
 		Component.leftWheelAccelerationCap.enable();
@@ -167,20 +165,20 @@ public class RobotMap {
 		Component.rightWheelAccelerationCap.enable();
 		// Wheels
 		Component.rightWheelA = new Motor("rightWheelA", false, Component.rightWheelAccelerationCap,
-				new CANTalonSRX(Port.CANMotor.rightDriveA));
+			new CANTalonSRX(Port.CANMotor.rightDriveA));
 		Component.rightWheelB = new Motor("rightWheelB", false, Component.rightWheelAccelerationCap,
-				new CANTalonSRX(Port.CANMotor.rightDriveB));
+			new CANTalonSRX(Port.CANMotor.rightDriveB));
 		Component.leftWheelA = new Motor("leftWheelA", true, Component.leftWheelAccelerationCap,
-				new CANTalonSRX(Port.CANMotor.leftDriveA));
+			new CANTalonSRX(Port.CANMotor.leftDriveA));
 		Component.leftWheelB = new Motor("leftWheelB", true, Component.leftWheelAccelerationCap,
-				new CANTalonSRX(Port.CANMotor.leftDriveB));
+			new CANTalonSRX(Port.CANMotor.leftDriveB));
 		// Shifter
 		Component.shifter = new SolenoidShifters(Port.Pneumatics.shifter.buildDoubleSolenoid());
 		// General Chassis
 		Component.chassis = new TankDriveShifting("2019-Chassis", Component.leftWheelA, Component.leftWheelB,
-				Component.rightWheelA, Component.rightWheelB, Component.shifter);
+			Component.rightWheelA, Component.rightWheelB, Component.shifter);
 		Component.drivePID = new CustomPIDController(PID.Drive.P, PID.Drive.I, PID.Drive.D, PID.Drive.F,
-				Component.chassisEncoders);
+			Component.chassisEncoders);
 		// Component.drivePID.setAbsoluteTolerance(PID.Drive.tolerance);
 		// Component.drivePID.setDerivativeTolerance(PID.Drive.dTolerance);
 		Component.chassisTurnPID = new CustomPIDController(PID.Turn.P, PID.Turn.I, PID.Turn.D, Component.navx);
@@ -192,35 +190,35 @@ public class RobotMap {
 		Component.leftElevatorMotor = new CANTalonSRX(Port.CANMotor.leftElevatorMotor);
 		Component.elevatorEncoder = new CANTalonEncoder(Component.leftElevatorMotor, FourBarElevator.TICK_MULTIPLIER);
 		Component.elevatorPID = new CustomPIDController(PID.Elevator.P, PID.Elevator.I, PID.Elevator.D, PID.Elevator.F,
-				Component.elevatorEncoder);
+			Component.elevatorEncoder);
 		Component.leftElevatorMotor.setNeutralMode(NeutralMode.Brake);
 		Component.rightElevatorMotor.setNeutralMode(NeutralMode.Brake);
 		// Component.elevatorPID.setAbsoluteTolerance(PID.Elevator.tolerance);
 		// Component.elevatorPID.setDerivativeTolerance(PID.Elevator.dTolerance);
 		Component.fourBar = new FourBarElevator(
-				new SolenoidSubsystem("FourBarLever", SolenoidState.RETRACT,
-						Port.Pneumatics.fourBarLever.buildDoubleSolenoid()),
-				new PositionSensorMotor("Elevator", Component.elevatorPID, Component.leftElevatorMotor,
-						Component.rightElevatorMotor));
+			new SolenoidSubsystem("FourBarLever", SolenoidState.RETRACT,
+				Port.Pneumatics.fourBarLever.buildDoubleSolenoid()),
+			new PositionSensorMotor("Elevator", Component.elevatorPID, Component.leftElevatorMotor,
+				Component.rightElevatorMotor));
 		Input.elevatorSwitchBottom = new CustomDigitalLimitSwitch(Port.Digital.elevatorSwitchBottomPort);
 		Input.elevatorSwitchTop = new CustomDigitalLimitSwitch(Port.Digital.elevatorSwitchTopPort);
-		Component.mainSubsystems = new Subsystem[] {};
 		/* Floorio */
 		Component.floorio = new FloorIO(
-				new SolenoidSubsystem("HatchOuttakePiston", SolenoidState.RETRACT,
-						Port.Pneumatics.hatchOuttakePiston.buildDoubleSolenoid()),
-				new SolenoidSubsystem("VelcroPiston", SolenoidState.RETRACT,
-						Port.Pneumatics.velcroPiston.buildDoubleSolenoid()),
-				new SolenoidSubsystem("Wrist", SolenoidState.RETRACT, Port.Pneumatics.wrist.buildDoubleSolenoid()),
-				new Motor("HatchRoller", new CANTalonSRX(Port.CANMotor.hatchRoller)),
-				new Motor("CargoRoller", new CANTalonSRX(Port.CANMotor.cargoRoller)));
-		// Human Input
+			new SolenoidSubsystem("HatchOuttakePiston", SolenoidState.RETRACT,
+				Port.Pneumatics.hatchOuttakePiston.buildDoubleSolenoid()),
+			new SolenoidSubsystem("VelcroPiston", SolenoidState.RETRACT,
+				Port.Pneumatics.velcroPiston.buildDoubleSolenoid()),
+			new SolenoidSubsystem("Wrist", SolenoidState.RETRACT, Port.Pneumatics.wrist.buildDoubleSolenoid()),
+			new Motor("HatchRoller", new CANTalonSRX(Port.CANMotor.hatchRoller)),
+			new Motor("CargoRoller", new CANTalonSRX(Port.CANMotor.cargoRoller)));
+		/* Human Input */
 		HumanInput.Driver.xbox = new CustomXbox(Port.HumanInput.xboxController);
 		HumanInput.Driver.xbox.setDeadZone(HumanInterfaceConfig.XBOX_DEADZONE);
 		HumanInput.Operator.joystick = new CustomJoystick(Port.HumanInput.joystick);
 		HumanInput.Operator.joystick.setDeadzone(HumanInterfaceConfig.JOYSTICK_DEADZONE);
-		Component.mainSubsystems = new Subsystem[] { Component.chassis, Component.fourBar.lever,
+		/* Main Subsystems */
+		Component.mainSubsystems = new Subsystem[] {Component.chassis, Component.fourBar.lever,
 				Component.fourBar.elevator, Component.floorio.hatchOuttakePiston, Component.floorio.velcroPiston,
-				Component.floorio.wrist, Component.floorio.hatchRoller, Component.floorio.cargoRoller };
+				Component.floorio.wrist, Component.floorio.hatchRoller, Component.floorio.cargoRoller};
 	}
 }
