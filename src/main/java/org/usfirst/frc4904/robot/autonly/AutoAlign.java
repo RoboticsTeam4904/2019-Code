2c9636ca1a7e690e0574cdb722f855ae344e590f
Evan Steirman <evastei@nuevaschool.org>
2019-03-03 16:09:20 -0800

    renamed boringauton to autoalign

--- src/main/java/org/usfirst/frc4904/robot/autonly/AutoAlign.java
+++ src/main/java/org/usfirst/frc4904/robot/autonly/AutoAlign.java
@@ -10,28 +10,28 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.usfirst.frc4904.standard.commands.RunIf;
 import org.usfirst.frc4904.standard.commands.RunIfElse;
 
-public class BoringAuton extends CommandGroup {
+public class AutoAlign extends CommandGroup {
 
-	public BoringAuton(DoubleSupplier betaSupplier, DoubleSupplier xSupplier, DoubleSupplier ySupplier) {
-		super("BoringAuton");
+	public AutoAlign(DoubleSupplier betaSupplier, DoubleSupplier xSupplier, DoubleSupplier ySupplier) {
+		super("AutoAlign");
 		addSequential(new RunIfElse(
-			new BoringAutonPosX(betaSupplier, xSupplier, ySupplier), //the left of the robot is towards the positive x direction
-			new BoringAutonNegX(betaSupplier, xSupplier, ySupplier),
+			new AutoAlignPosX(betaSupplier, xSupplier, ySupplier), //the left of the robot is towards the positive x direction
+			new AutoAlignNegX(betaSupplier, xSupplier, ySupplier),
 			() -> xSupplier.getAsDouble() >= 0
 		));
 		addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, () -> ySupplier.getAsDouble(), RobotMap.Component.drivePID));
 	}
-	public class BoringAutonPosX extends CommandGroup {
-		public BoringAutonPosX(DoubleSupplier betaSupplier, DoubleSupplier xSupplier, DoubleSupplier ySupplier) {
-			super("BoringAutonPosX"); //chassisturn and beta are positive to the right
+	public class AutoAlignPosX extends CommandGroup {
+		public AutoAlignPosX(DoubleSupplier betaSupplier, DoubleSupplier xSupplier, DoubleSupplier ySupplier) {
+			super("AutoAlignPosX"); //chassisturn and beta are positive to the right
 			addSequential(new ChassisTurn(RobotMap.Component.chassis, () -> 90 + betaSupplier.getAsDouble(), RobotMap.Component.navx, null, RobotMap.Component.chassisTurnMC));
 			addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, () -> xSupplier.getAsDouble(), RobotMap.Component.drivePID));
 			addSequential(new ChassisTurn(RobotMap.Component.chassis, -90, RobotMap.Component.navx, null, RobotMap.Component.chassisTurnMC));
 		}
 	}
-	public class BoringAutonNegX extends CommandGroup {
-		public BoringAutonNegX(DoubleSupplier betaSupplier, DoubleSupplier xSupplier, DoubleSupplier ySupplier) {
-			super("BoringAutonNegX");
+	public class AutoAlignNegX extends CommandGroup {
+		public AutoAlignNegX(DoubleSupplier betaSupplier, DoubleSupplier xSupplier, DoubleSupplier ySupplier) {
+			super("AutoAlignNegX");
 			addSequential(new ChassisTurn(RobotMap.Component.chassis, () -> -90 + betaSupplier.getAsDouble(), RobotMap.Component.navx, null, RobotMap.Component.chassisTurnMC));
 			addSequential(new ChassisMoveDistance(RobotMap.Component.chassis, () -> -xSupplier.getAsDouble(), RobotMap.Component.drivePID));
 			addSequential(new ChassisTurn(RobotMap.Component.chassis, 90, RobotMap.Component.navx, null, RobotMap.Component.chassisTurnMC));
