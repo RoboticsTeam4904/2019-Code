eb40aee6017f389ceb825844c34d9efece536d59
Evan Steirman <evastei@nuevaschool.org>
2019-03-09 15:21:08 -0800

    remade hatch commands

--- src/main/java/org/usfirst/frc4904/robot/commands/manipulator/ReadyHatchIntake.java
+++ src/main/java/org/usfirst/frc4904/robot/commands/manipulator/ReadyHatchIntake.java
@@ -1,20 +1,25 @@
 package org.usfirst.frc4904.robot.commands.manipulator;
 
 import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.standard.commands.RunIf;
+import org.usfirst.frc4904.standard.subsystems.SolenoidSubsystem.SolenoidState;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 /**
  * Gets robot ready to intake hatch
  */
-public class ReadyHatchIntake extends CommandGroup {
+public class ReadyHatchIntake extends RunIf {
 	public ReadyHatchIntake() {
-		super("HatchIntake");
-		requires(RobotMap.Component.manipulator.arm);
-		requires(RobotMap.Component.manipulator.claws);
-		requires(RobotMap.Component.manipulator.grabber);
-		requires(RobotMap.Component.manipulator.hatchExtender);
-		requires(RobotMap.Component.manipulator.roller);
-		addParallel(new ManipulatorArmUp());
-		addParallel(new ManipulatorClawsUp());
-		addParallel(new HatchExtenderIn());
+		super(
+			new ReadyHatchIntakeDangerously(),
+			() -> RobotMap.Component.manipulator.claws.getState() == SolenoidState.RETRACT
+		);
+	}
+	public static class ReadyHatchIntakeDangerously extends CommandGroup {
+		public ReadyHatchIntakeDangerously() {
+			addParallel(new ManipulatorArmUp());
+			addParallel(new ManipulatorClawsUp());
+			addParallel(new HatchExtenderOut());
+			addParallel(new ManipulatorGrabberIn());
+		}
 	}
 }
