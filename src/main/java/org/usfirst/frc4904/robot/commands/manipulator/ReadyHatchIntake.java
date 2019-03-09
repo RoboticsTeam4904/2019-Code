3f031da054dc52e70b2e1c0cabf2bbc1aab9ec01
Evan Steirman <evastei@nuevaschool.org>
2019-03-09 15:51:08 -0800

    moved hatchintake into readyhatchintake

--- src/main/java/org/usfirst/frc4904/robot/commands/manipulator/ReadyHatchIntake.java
+++ src/main/java/org/usfirst/frc4904/robot/commands/manipulator/ReadyHatchIntake.java
@@ -3,6 +3,7 @@ package org.usfirst.frc4904.robot.commands.manipulator;
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.standard.commands.RunIf;
 import org.usfirst.frc4904.standard.subsystems.SolenoidSubsystem.SolenoidState;
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 /**
  * Gets robot ready to intake hatch
@@ -20,6 +21,19 @@ public class ReadyHatchIntake extends RunIf {
 			addParallel(new ManipulatorClawsUp());
 			addParallel(new HatchExtenderOut());
 			addParallel(new ManipulatorGrabberIn());
+		}
+		@Override
+		protected void interrupted() {
+			Command hatchIntake = new HatchIntake();
+			hatchIntake.start();
+		}
+	}
+	public static class HatchIntake extends RunIf {
+		public HatchIntake() {
+			super(
+				new ManipulatorGrabberOut(),
+				() -> RobotMap.Component.manipulator.claws.getState() == SolenoidState.RETRACT
+			);
 		}
 	}
 }
