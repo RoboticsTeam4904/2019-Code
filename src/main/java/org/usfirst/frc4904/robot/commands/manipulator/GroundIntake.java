4bb7fe5b68d00702df7993a27ae804acfffde98d
Evan Steirman <evastei@nuevaschool.org>
2019-03-09 12:20:26 -0800

    started to make it work w/ time

--- src/main/java/org/usfirst/frc4904/robot/commands/manipulator/GroundIntake.java
+++ src/main/java/org/usfirst/frc4904/robot/commands/manipulator/GroundIntake.java
@@ -1,10 +1,11 @@
 package org.usfirst.frc4904.robot.commands.manipulator;
 
+import org.usfirst.frc4904.standard.commands.RunFor;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class GroundIntake extends CommandGroup {
 	public GroundIntake() {
-		addSequential(new ManipulatorGroundOut());
+		addSequential(new RunFor(new ManipulatorGroundOut(), 0.5));
 		addSequential(new ManipulatorRollerIn());
 		addSequential(new ManipulatorGroundIn());
 		addSequential(new ManipulatorRollerStop());
