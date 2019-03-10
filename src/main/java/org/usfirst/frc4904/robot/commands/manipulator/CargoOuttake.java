905e787b3991aa6f2366bb848a933392e79d0b00
Osher Lerner <oshlern@nuevaschool.org>
2019-03-10 13:38:55 -0700

    new cargo outtake now brings claws up

--- src/main/java/org/usfirst/frc4904/robot/commands/manipulator/CargoOuttake.java
+++ src/main/java/org/usfirst/frc4904/robot/commands/manipulator/CargoOuttake.java
@@ -3,13 +3,29 @@ package org.usfirst.frc4904.robot.commands.manipulator;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
+import org.usfirst.frc4904.robot.RobotMap;
+import edu.wpi.first.wpilibj.command.Command;
 
-public class CargoOuttake extends CommandGroup {
-	public final double CLAW_WAIT_TIME = 2;
+public class CargoOuttake extends RollerOut {
 
 	public CargoOuttake() {
-		addParallel(new RollerOut());
-		addSequential(new WaitCommand(CLAW_WAIT_TIME));
-		addSequential(new ClawsUp());
+		super();
+	}
+
+	@Override
+	protected void interrupted() {
+		Command finishOuttake = new FinishCargoOuttake();
+		finishOuttake.start();
+	}
+
+	public static class FinishCargoOuttake extends CommandGroup {
+		public final double CLAW_WAIT_TIME = 2;
+
+		public FinishCargoOuttake() {
+			// Interruptable by other roller commands (due to requires)
+			requires(RobotMap.Component.manipulator.roller);
+			addSequential(new WaitCommand(CLAW_WAIT_TIME));
+			addSequential(new ClawsUp());
+		}
 	}
 }
--- src/main/java/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
+++ src/main/java/org/usfirst/frc4904/robot/humaninterface/operators/DefaultOperator.java
@@ -44,35 +44,20 @@ public class DefaultOperator extends Operator {
 		// RobotMap.HumanInput.Operator.joystick.button2.whenPressed(
 		// new RunIfElse(new WristUp(), new WristDown(),
 		// RobotMap.Component.floorio.wrist::isExtended));
-		// RobotMap.HumanInput.Operator.joystick.button3.onlyWhileHeld(new
-		// HatchRollerIntake());
-		// RobotMap.HumanInput.Operator.joystick.button4.onlyWhileHeld(new
-		// CargoIntake());
-		// RobotMap.HumanInput.Operator.joystick.button5.whenPressed(new
-		// VelcroPistonExtend());
-		// RobotMap.HumanInput.Operator.joystick.button5.whenReleased(new
-		// VelcroPistonRetract());
-		// RobotMap.HumanInput.Operator.joystick.button6.onlyWhileHeld(new
-		// CargoOuttake());
-		// RobotMap.HumanInput.Operator.joystick.button7.onlyWhileHeld(new
-		// HatchRollerOuttake());
 		RobotMap.HumanInput.Operator.joystick.button1.onlyWhileHeld(new RollerOut());
 		RobotMap.HumanInput.Operator.joystick.button2.onlyWhileHeld(new RollerIn());
+
 		RobotMap.HumanInput.Operator.joystick.button3.onlyWhileHeld(new HatchIntake());
-		RobotMap.HumanInput.Operator.joystick.button4.onlyWhileHeld(new CargoIntake());
 		RobotMap.HumanInput.Operator.joystick.button5.whenPressed(new HatchOuttake());
+		RobotMap.HumanInput.Operator.joystick.button4.onlyWhileHeld(new CargoIntake());
+		RobotMap.HumanInput.Operator.joystick.button6.onlyWhileHeld(new CargoOuttake());
+
 		RobotMap.HumanInput.Operator.joystick.button7.whenPressed(new HatchExtenderIn());
 		RobotMap.HumanInput.Operator.joystick.button8.whenPressed(new HatchExtenderOut());
 		RobotMap.HumanInput.Operator.joystick.button11.whenPressed(new ClawsUp());
 		RobotMap.HumanInput.Operator.joystick.button12.whenPressed(new ClawsDown());
-		// RobotMap.HumanInput.Operator.joystick.button2.onlyWhileHeld(
-		// new MotorConstant(RobotMap.Component.manipulator.roller, RobotMap.Component.manipulator.INTAKE_SPEED));
-		// RobotMap.HumanInput.Operator.joystick.button1.onlyWhileHeld(
-		// new MotorConstant(RobotMap.Component.manipulator.roller, RobotMap.Component.manipulator.OUTTAKE_SPEED));
 		// RobotMap.HumanInput.Operator.joystick.button11.whenPressed(new WristUp());
 		// RobotMap.HumanInput.Operator.joystick.button12.whenPressed(new WristDown());
-		// RobotMap.HumanInput.Operator.joystick.button5.whenPressed(new ClawsUp());
-		// RobotMap.HumanInput.Operator.joystick.button6.whenPressed(new ClawsDown());
 		// RobotMap.HumanInput.Operator.joystick.button8
 		// .whenPressed(new RunIfElse(new SolenoidRetract("FourBarUp",
 		// RobotMap.Component.fourBar.lever),
@@ -92,9 +77,5 @@ public class DefaultOperator extends Operator {
 		// OverrideEnable(RobotMap.Component.fourBar));
 		// RobotMap.HumanInput.Operator.joystick.button9.whenReleased(new
 		// OverrideDisable(RobotMap.Component.fourBar));
-		// RobotMap.HumanInput.Operator.joystick.button10.whenPressed(new
-		// HatchOuttakeExtend());
-		// RobotMap.HumanInput.Operator.joystick.button10.whenReleased(new
-		// HatchOuttakeRetract());
 	}
 }
