9c584a1ae707e27ce927540d68d49d1c38cac27d
Osher Lerner <oshlern@nuevaschool.org>
2019-03-11 17:02:32 -0700

    new elevator control with continuous checks

--- src/main/java/org/usfirst/frc4904/robot/commands/elevator/ElevatorControl.java
+++ src/main/java/org/usfirst/frc4904/robot/commands/elevator/ElevatorControl.java
@@ -4,30 +4,33 @@ import java.util.function.BooleanSupplier;
 
 import org.usfirst.frc4904.robot.RobotMap;
 import org.usfirst.frc4904.robot.subsystems.FourBarElevator;
-import org.usfirst.frc4904.standard.commands.RunIf;
+import org.usfirst.frc4904.standard.LogKitten;
 import org.usfirst.frc4904.standard.commands.motor.MotorControl;
 import org.usfirst.frc4904.standard.custom.controllers.CustomJoystick;
 
 /**
  * Control elevator manually
  */
-public class ElevatorControl extends RunIf {
-	public ElevatorControl(BooleanSupplier joystickDirection) {
-		super(new MotorControl(RobotMap.Component.fourBar.elevator, RobotMap.HumanInput.Operator.joystick,
-				CustomJoystick.Y_AXIS,
-				joystickDirection.getAsBoolean() ? FourBarElevator.UP_SPEED : FourBarElevator.DOWN_SPEED), () -> {
-					if (joystickDirection.getAsBoolean()) {
-						return !RobotMap.Input.elevatorSwitchTop.get() || RobotMap.Component.fourBar.isOverridden();
-					} else {
-						return !RobotMap.Input.elevatorSwitchBottom.get() || RobotMap.Component.fourBar.isOverridden();
+public class ElevatorControl extends MotorControl {
+	private BooleanSupplier joystickDirection;
 
-					}
-				});
+	public ElevatorControl(BooleanSupplier joystickDirection) {
+		super(RobotMap.Component.fourBar.elevator, RobotMap.HumanInput.Operator.joystick,
+				CustomJoystick.Y_AXIS);
 		requires(RobotMap.Component.fourBar.elevator);
 	}
 
 	@Override
-	protected boolean isFinished() {
-		return false;
+	protected void execute() {
+		if (!RobotMap.Component.fourBar.isOverridden() && (
+				(joystickDirection.getAsBoolean() && RobotMap.Input.elevatorSwitchTop.get())
+				|| (!joystickDirection.getAsBoolean() && RobotMap.Input.elevatorSwitchBottom.get()))) {
+				motor.set(0);
+				LogKitten.d("MotorControl executing: 0");
+		} else {
+			double scale = joystickDirection.getAsBoolean() ? FourBarElevator.UP_SPEED : FourBarElevator.DOWN_SPEED;
+			motor.set(controller.getAxis(axis) * scale);
+			LogKitten.d("MotorControl executing: " + controller.getAxis(axis));
+		}
 	}
 }
